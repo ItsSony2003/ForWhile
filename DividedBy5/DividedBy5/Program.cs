eed43ecfc4a461f6e15e59653424a19c0213d0a7
ItsSony2003 <sonythaiduong2003@gmail.com>
2024-11-23 19:55:15 +0700

    Create Program.cs

---
+++ DividedBy5/DividedBy5/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DividedBy5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Numbers That Can Be Divided By 5 is: ");
+            for (int i = 0;  i <= 100;  i++)
+            {
+                if (i % 5 == 0) 
+                {
+                    Console.WriteLine("" + i);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
