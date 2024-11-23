87b24a2fab8d718da855f6eb73ce620c317986d1
ItsSony2003 <sonythaiduong2003@gmail.com>
2024-11-23 21:22:11 +0700

    Create Program.cs

---
+++ PrimeNumber/PrimeNumber/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = 0;
+
+            Console.WriteLine("Prime Numbers Are:");
+            for (int i = 2; i <= 100; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    if (i % j == 0)
+                    {
+                        count++;
+                    }
+                }
+
+                if (count == 2)
+                {
+                    Console.WriteLine(i);
+                }
+                count = 0;
+            }
+            Console.ReadKey();
+        }
+    }
+}
