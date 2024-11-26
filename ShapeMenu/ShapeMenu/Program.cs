12c9541af87116315f897c8a58ee85c02e42eabd
ItsSony2003 <sonythaiduong2003@gmail.com>
2024-11-26 16:22:46 +0700

    Create Program.cs

---
+++ ShapeMenu/ShapeMenu/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShapeMenu
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while(true)
+            {
+                Console.WriteLine("Shape Menu!");
+                Console.WriteLine("1. Print A Rectangle");
+                Console.WriteLine("2. Print A Square Triangle (The corner is square at \n4 different angles: top-left, top-right, bottom-left, bottom-right))");
+                Console.WriteLine("3. Print A Isosceles Triangle");
+                Console.WriteLine("4. Exit");
+                Console.WriteLine();
+
+                Console.Write("Enter Your Choice (1-4): ");
+                string input = Console.ReadLine();
+                int choice;
+
+                // Check if input is a valid integer
+                if (!int.TryParse(input, out choice))
+                {
+                    Console.WriteLine("Invalid choice! Please enter a number between 1 and 4.");
+                    continue;
+                }
+                switch (choice)
+                {
+                    case 1:
+                        break;
+                }
+            }
+        }
+    }
+}
